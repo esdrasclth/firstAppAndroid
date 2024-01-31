e58b3ec21080d482c809a44f77dd8874cdc013e2
Esdras Clother <Esdras.Clother@outlook.com>
2024-01-30 20:06:24 -0600

    connection to the SQLite database, allowing you to register the first name, last name, age and email fields

---
+++ app/src/main/java/Configuracion/Transacciones.java
@@ -0,0 +1,31 @@
+package Configuracion;
+
+public class Transacciones {
+
+    // Nombre de la base de datos
+    public static final String DBName = "PM012024";
+
+    // Creacion de las tablas de las bases de datos
+    public static final String TablePersonas = "personas";
+
+    // Creacion de los campos de la base de datos
+    public static final String id = "id";
+    public static final String nombres = "nombres";
+    public static final String apellidos = "apellidos";
+    public static final String edad = "edad";
+    public static final String correo = "correo";
+
+    // Creación de la sentencia SQL para crear la tabla personas
+    public static final String CreateTablePersonas = "CREATE TABLE " + TablePersonas + " ("
+            + id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + nombres + " TEXT, "
+            + apellidos + " TEXT, "
+            + edad + " INTEGER, "
+            + correo + " TEXT);";
+
+    // Sentencia SQL para eliminar la tabla personas
+    public static final String DropTablePersonas = "DROP TABLE IF EXISTS " + TablePersonas + ";";
+
+    // Sentencia SQL para seleccionar todos los registros de la tabla personas
+    public static final String SelectAllPersonas = "SELECT * FROM " + TablePersonas + ";";
+}
--- app/src/main/java/com/example/firstappandroid/MainActivity.java
+++ app/src/main/java/com/example/firstappandroid/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.firstappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import Configuracion.SQLiteConexion;
+import Configuracion.Transacciones;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText nombre, apellido, telefono;
+    EditText nombres, apellido, edad, correo;
     Button enviar;
 
     @Override
@@ -19,33 +23,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nombre = findViewById(R.id.txtNombre);
+        nombres = findViewById(R.id.txtNombre);
         apellido = findViewById(R.id.txtApellido);
-        telefono = findViewById(R.id.txtTelefono);
+        edad = findViewById(R.id.txtEdad);
+        correo = findViewById(R.id.txtEmail);
         enviar = findViewById(R.id.btnEnviar);
 
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                String nombreValor = nombre.getText().toString();
-                String apellidoValor = apellido.getText().toString();
-                String telefonoValor = telefono.getText().toString();
-
-                if (nombreValor.isEmpty() || apellidoValor.isEmpty() || telefonoValor.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
-                } else {
-                    Bundle enviarDatos = new Bundle();
-                    enviarDatos.putString("keyNombre", nombreValor);
-                    enviarDatos.putString("keyApellido", apellidoValor);
-                    enviarDatos.putString("keyTelefono", telefonoValor);
-
-                    Intent intent = new Intent(MainActivity.this, ActivityWelcome.class);
-                    intent.putExtras(enviarDatos);
-
-                    startActivity(intent);
-                }
+                AddPerson();
             }
         });
+    }
+
+    private void AddPerson() {
+        SQLiteConexion conexion = new SQLiteConexion(this, Transacciones.DBName, null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+
+        ContentValues valores = new ContentValues();
+        valores.put(Transacciones.nombres, nombres.getText().toString());
+        valores.put(Transacciones.apellidos, apellido.getText().toString());
+        valores.put(Transacciones.edad, edad.getText().toString());
+        valores.put(Transacciones.correo, correo.getText().toString());
+
+        long resultado = db.insert(Transacciones.TablePersonas, Transacciones.id, valores);
+
+        if (resultado > 0) {
+            Toast.makeText(getApplicationContext(), "Registro insertado correctamente", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Error al insertar el registro", Toast.LENGTH_LONG).show();
+        }
+
+        // Cierra la conexión a la base de datos
+        db.close();
+
+        // Limpia los campos después de la inserción
+        nombres.setText("");
+        apellido.setText("");
+        edad.setText("");
+        correo.setText("");
     }
 }
